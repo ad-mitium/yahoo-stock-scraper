90eb1b7795510b3c5d992c7816748dd9d2a6b224
ad-mitium <99030354+ad-mitium@users.noreply.github.com>
2022-09-09 18:04:23 -0400

    Variable renaming to avoid global/local issues

--- lib/version.py
+++ lib/version.py
@@ -1,7 +1,7 @@
 
 def ver_info(version_info):
-    version_number = '.'.join(str(c) for c in version_info)
-    version = 'ver. '+version_number
+    revision_number = '.'.join(str(c) for c in version_info)
+    version = 'ver. '+revision_number
     return version
 
 
