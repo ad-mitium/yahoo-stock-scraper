93f3a976b2a960637c4903767282d58d976c80c5
ad-mitium <99030354+ad-mitium@users.noreply.github.com>
2022-11-10 12:32:39 -0500

    Bump Chrome agent to 107.0.5304.107

--- lib/scrape.py
+++ lib/scrape.py
@@ -7,7 +7,7 @@ def get_page(url_to_scrape, is_unit_test=False):
 
     # Pretend to be Chrome on Windows 10
     headers = { 
-        'User-Agent'      : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.91 Safari/537.36', 
+        'User-Agent'      : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.5304.107 Safari/537.36', 
         'Accept'          : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
         'Accept-Language' : 'en-US,en;q=0.5',
         'DNT'             : '1', # Do Not Track Request Header 
