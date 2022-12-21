64298214d3db4db0523c380ca25a112003c660da
ad-mitium <99030354+ad-mitium@users.noreply.github.com>
2022-12-21 14:00:39 -0500

    Change Chrome user agent to generic UA string

--- lib/scrape.py
+++ lib/scrape.py
@@ -7,7 +7,7 @@ def get_page(url_to_scrape, is_unit_test=False):
 
     # Pretend to be Chrome on Windows 10
     headers = { 
-        'User-Agent'      : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.124 Safari/537.36', 
+        'User-Agent'      : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36', 
         'Accept'          : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
         'Accept-Language' : 'en-US,en;q=0.5',
         'DNT'             : '1', # Do Not Track Request Header 
